588fe23a0f2add5e6a495d93a13eac6c9adaae8c
Johnson Ojo <johnsonojo89@gmail.com>
2018-08-14 23:27:47 +0100

    endpoint to get a question

--- server/v1/controllers/questioncontroller.js
+++ server/v1/controllers/questioncontroller.js
@@ -6,4 +6,23 @@ export default class Question {
     static getAllQuestions(req, res) {
         return res.status(200).json({ questions: questionDatabase.questions });
     }
+
+    // get a question
+    static getOneQuestion(req, res) {
+        const { questionId } = req.params;
+        questionDatabase.questions.forEach((value) => {
+            if (value.id === parseInt(questionId, 10)) {
+                return res.status(200).json({
+                    value,
+                    message: 'success',
+                    error: false,
+                });
+            }
+        });
+        return res.status(404).json({
+            messsage: 'Question not found',
+            Error: true,
+        });
+    }
+
 }
